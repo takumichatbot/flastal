eebac73473dfd9309b1a726c15860b3388e3c18a
takumi120 <chatbotaig0@gmail.com>
2025-12-15 18:10:10 +0900

    A

--- client/src/app/organizers/dashboard/page.js
+++ client/src/app/organizers/dashboard/page.js
@@ -1,7 +1,6 @@
 // /organizers/dashboard/page.js
 'use client';
 
-import { useState, useEffect } from 'react';
 import { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
