9d4c228b4cf69fbba392d7d54bac68cda815e401
takumi120 <chatbotaig0@gmail.com>
2025-12-23 06:00:19 +0900

    A

--- client/src/app/mypage/edit/page.js
+++ client/src/app/mypage/edit/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+export const dynamic = 'force-dynamic';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '../../contexts/AuthContext';
 import { useForm } from 'react-hook-form';
