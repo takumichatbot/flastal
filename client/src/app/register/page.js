ad22fb50de77d784c18d0d7fdcaf323fd23c0d7a
takumi120 <chatbotaig0@gmail.com>
2025-12-23 05:47:14 +0900

    A

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from 'react';
+export const dynamic = 'force-dynamic';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '../contexts/AuthContext';
 import Link from 'next/link';
