ec6cbc2b9c9dbb818564b4b19c5c2e3a97b161ef
Takumi <takuminsitou946@gmail.com>
2026-06-30 19:10:18 +0900

    fix: CSPにlarubot.tokyoを追加してブログ埋め込みスクリプトを許可

--- client/next.config.js
+++ client/next.config.js
@@ -53,7 +53,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://js.stripe.com https://cdn.growthbook.io",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://js.stripe.com https://cdn.growthbook.io https://larubot.tokyo",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https: http:",
